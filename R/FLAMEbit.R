508f15fb1f7a8b22cc4007c94cdaa9d4428ad202
Jerry Chia-Rui Chang <[chiarui424@gmail.com]>
2018-05-24 00:00:40 -0400

    Convert Data Type

--- R/FLAMEbit.R
+++ R/FLAMEbit.R
@@ -12,7 +12,9 @@
 
 FLAME_bit <- function(df, holdout, covs, covs_max_list,num_treated,num_control) {
   df <- data.frame(df)
+  covs <- as.integer(covs)
   holdout <- data.frame(holdout)
+  covs_max_list <- as.integer(covs_max_list)
   source_python("run_bit.py")
   return(run_bit(r_to_py(df),r_to_py(holdout),covs,covs_max_list,as.integer(num_treated),
                  as.integer(num_control)))
